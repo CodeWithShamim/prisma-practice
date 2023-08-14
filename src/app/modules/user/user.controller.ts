70002b3e66b7572ed2d5b73a0770d27c7a49687e
codewithshamim <shamimislamonline@gmail.com>
2023-08-14 13:31:31 +0600

    custom reponse added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,11 +1,18 @@
+import { User } from "@prisma/client";
 import catchAsync from "../../../utils/catchAsync";
+import sendResponse from "../../../utils/sendResponse";
 import { UserService } from "./user.service";
 
 const createUser = catchAsync(async (req, res) => {
   const userData = req.body;
   const result = await UserService.createUser(userData);
 
-  res.send(result);
+  sendResponse<User>(res, {
+    success: true,
+    statusCode: 200,
+    message: "User created successfully",
+    data: result,
+  });
 });
 
 export const UserController = {
