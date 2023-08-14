49704bb036da8f13f5d62f0a76f0f1fe5c03ab1b
codewithshamim <shamimislamonline@gmail.com>
2023-08-14 11:51:12 +0600

    client added

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 async function main() {
-  console.log("Test");
+  const result = await prisma.user.findMany();
+  console.log(result);
+
+  // const result = await prisma.user.create({
+  //   data: {
+  //     email: "shamim2@gmail.com",
+  //     name: "shamim2",
+  //   },
+  // });
+
+  console.log(result);
 }
 
 main();
